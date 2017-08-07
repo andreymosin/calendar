195c41040908eb62c9c8ee9260378d42c95bf745
Andrey Mosin <andrey.mosin@cleverlance.com>
2017-08-07 22:07:07 +0200

    first commit

---
+++ CalendarView/WeekDayView.swift
@@ -0,0 +1,21 @@
+//
+//  WeekDayView.swift
+//  CalendarView
+//
+//  Created by Andrey Mosin on 07/08/2017.
+//  Copyright © 2017 Andrey Mosin. All rights reserved.
+//
+
+import UIKit
+
+class WeekDayView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
